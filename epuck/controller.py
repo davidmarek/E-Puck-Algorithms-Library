493edbf96aa566561a6b267a4fd208d9ebd5f310
David Marek <davidm@atrey.karlin.mff.cuni.cz>
2010-03-12 09:37:23 +0100

    Changed comments and added left_motor_speed.

--- epuck/controller.py
+++ epuck/controller.py
@@ -7,26 +7,55 @@ import serial
 import sys
 import logging
 
-class Controller:
+class EPuckError(Exception):
+    """Base class exception for this library."""
 
+    def __init__(self, message):
+        """Create E-Puck exception.
+
+        Arguments:
+            message -- Description of what went wrong.
+
+        """
+        self.message = message
+
+    def __str__(self):
+        return self.message
+
+class ControllerError(EPuckError):
+    """Controller exception, E-puck robot probably not responding."""
+    pass
+
+class Controller(object):
     """Control E-Puck robot.
 
     Give commands to e-puck robot via bluetooth connection. Use BTcom 
     tool and its text protocol. 
 
+    Atributes:
+        MAX_SPEED -- Maximum speed of e-puck's motors measured in 
+                     pulses per second. -MAX_SPEED is maximum backward 
+                     speed.
+
     """
+
+    MAX_SPEED = 1000 
 
     def __init__(self, port):
         """Create new connection with e-puck robot. 
 
-        Keyword arguments:
-        port -- port where the robot is connected, e.g. /dev/rfcomm0.
+        Arguments:
+            port -- port where the robot is connected, e.g. /dev/rfcomm0.
 
         """
-        # All messages are logged using the logging module.
-        self.logger = logging.getLogger('EPuck.controller.Controller')
+        #Initialize atributes
+        self._left_motor_speed = 0  # Speed of left wheel
+        self._right_motor_speed = 0 # Speed of right wheel
 
-        # Creates serial connection, 
+        # All messages are logged using the logging module.
+        self.logger = logging.getLogger('epuck.controller.Controller')
+
+        # Create serial connection to e-puck. 
         try:
             self.serial_connection = serial.Serial(
                 port=port,       # Port, where e-puck is connected.
@@ -36,7 +65,51 @@ class Controller:
             )
         except serial.serialutil.SerialException:
             self.logger.error("E-Puck seems to be offline.")
-            sys.exit(1)
+            raise ControllerError("E-Puck seems to be offline.")
         self.logger.info("E-Puck connected.")
 
+    @property
+    def left_motor_speed(self):
+        """The speed of left motor. 
+        
+        The speed is measured in pulses per second, one pulse is 
+        aproximately 0.13mm. The speed can be in range 
+        (-MAX_SPEED, MAX_SPEED). 
+        
+        It could have been saved only when changed and this method could
+        just return internal representation, but it's more reliable to 
+        actually read the speed from e-puck's memory.
+
+        """
+        self.logger.debug("Command: E.")
+        # Send command to e-puck.
+        self.serial_connection.write("E\n")
+        # Read response from e-puck.
+        response = self.serial_connection.readline() 
+        self.logger.debug("Answer: "+response)
+        # The response should be in format: "e,left_speed,right_speed".
+        # Check the response and set the internal representation.
+        resp_token = response.split(",")
+        if len(resp_token) == 3 && resp_token[0] == "e":
+            self._left_motor_speed = int(resp_token[1])
+            return self._left_motor_speed
+        else:
+            self.logger.error("Wrong answer from e-puck")
+            raise ControllerError("Wrong answer from e-puck.")
+
+    @left_motor_speed.setter
+    def left_motor_speed(self, new_speed):
+        self.logger.debug("Command: D,%d,%d" % (new_speed, _right_motor_speed))
+        # Send command to e-puck.
+        self.serial_connection.write("D,%d,%d\n" % 
+            (new_speed, _right_motor_speed))
+        # Read response from e-puck.
+        response = self.serial_connection.readline()
+        self.logger.debug("Answer: "+response)
+        # The response should be "d".
+        if response.startswith("d"):
+            self._left_motor_speed = new_speed
+        else:
+            self.logger.error("Wrong answer from e-puck")
+            raise ControllerException("Wrong answer from e-puck")
 
